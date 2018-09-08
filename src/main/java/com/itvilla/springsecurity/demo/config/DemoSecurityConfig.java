886c80484a4b0a263014daa66194ccbebde1023e
naren <naren.kang@gmail.com>
2018-09-08 16:14:00 +0530

    firstcommit

---
+++ src/main/java/com/itvilla/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.itvilla.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("naren").password("test123").roles("CUSTOMER"))
+			.withUser(users.username("amit").password("test123").roles("LEADER"))
+			.withUser(users.username("tejas").password("test123").roles("ADMIN"));
+	}
+	
+}
+
+
+
+
+
+
